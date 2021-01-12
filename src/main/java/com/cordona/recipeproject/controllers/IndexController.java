6dc49fecb7ec067c68e516b89761c81545e819e5
VentsislavStoevski <67237998+VentsislavStoevski@users.noreply.github.com>
2021-01-12 15:03:14 +0200

    Added named queries in the CategoryRepository and UnitOfMeasureRepository just for testing purposes.

--- src/main/java/com/cordona/recipeproject/controllers/IndexController.java
+++ src/main/java/com/cordona/recipeproject/controllers/IndexController.java
@@ -1,14 +1,22 @@
 package com.cordona.recipeproject.controllers;
 
+import com.cordona.recipeproject.domain.Category;
+import com.cordona.recipeproject.domain.UnitOfMeasure;
+import com.cordona.recipeproject.repositories.CategoryRepository;
+import com.cordona.recipeproject.repositories.UnitOfMeasureRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String indexPage(Model model) {
+
         return "index";
     }
 }
--- src/main/java/com/cordona/recipeproject/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/cordona/recipeproject/repositories/UnitOfMeasureRepository.java
@@ -4,6 +4,11 @@ import com.cordona.recipeproject.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 @Repository
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
